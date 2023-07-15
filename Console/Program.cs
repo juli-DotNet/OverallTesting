69c4dc530a403a877c86fd3689095dd096836d78
juliant <julian.tabaku@outlook.com>
2023-07-15 21:07:34 +0200

    add console project.

---
+++ Console/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Console;
+
+var testing = new ValidateArguments();
+var argsValue=new List<string>(){"--count", "name"};
+var result=testing.Validate(argsValue.ToArray());
+System.Console.WriteLine(result);
